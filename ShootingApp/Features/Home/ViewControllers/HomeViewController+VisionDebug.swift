c8add99ae731812ed22ef5714ababb41cd034fb8
Jose Catala <jfca68@gmail.com>
2024-11-28 20:34:30 +0300

    Added new Debug view to show an square around the person detected after shooting

--- ShootingApp/Features/Home/ViewControllers/HomeViewController+VisionDebug.swift
+++ ShootingApp/Features/Home/ViewControllers/HomeViewController+VisionDebug.swift
@@ -6,22 +6,31 @@
 //
 
 import UIKit
+import AVFoundation
 
-extension HomeViewController {
+extension HomeViewController: AntiCheatDebugDelegate {
     func setupDebugViews() {
-        debugHitView = DebugView(frame: .zero)
-        debugHitView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(debugHitView)
-        
-        NSLayoutConstraint.activate([
-            debugHitView.centerXAnchor.constraint(equalTo: crosshairView.centerXAnchor),
-            debugHitView.centerYAnchor.constraint(equalTo: crosshairView.centerYAnchor),
-            debugHitView.widthAnchor.constraint(equalToConstant: 100),
-            debugHitView.heightAnchor.constraint(equalToConstant: 100)
-        ])
+        visionDebugView = VisionDebugView(frame: .zero)
+        view.addSubview(visionDebugView)
+        AntiCheatSystem.shared.debugDelegate = self
     }
     
-    func showDebugHitMarker() {
-        debugHitView.showFor()
+    func showDebugRect(_ visionRect: CGRect) {
+        DispatchQueue.main.async {
+            let viewRect = self.convertFromVisionToView(visionRect)
+            self.visionDebugView.showAt(rect: viewRect)
+        }
+    }
+    
+    private func convertFromVisionToView(_ visionRect: CGRect) -> CGRect {
+        let viewBounds = view.bounds
+        
+        // Vision coordinates are normalized (0-1) and inverted vertically
+        let x = visionRect.minX * viewBounds.width
+        let y = (1 - visionRect.maxY) * viewBounds.height
+        let width = visionRect.width * viewBounds.width
+        let height = visionRect.height * viewBounds.height
+        
+        return CGRect(x: x, y: y, width: width, height: height)
     }
 }
